497bdaf2b9b7837135b8170a144b72257bf8b6a4
mahdi mehrabi <mahdimehrabi17@gmail.com>
2023-12-20 13:15:58 +0330

    replace x-server-ip-address with command endpoint ip address

--- main.go
+++ main.go
@@ -6,14 +6,19 @@ import (
 	"github.com/google/uuid"
 	"io"
 	"net/http"
+	"net/url"
 	"time"
 )
 
 func main() {
+	dataConnectionEndpoint := "http://172.24.90.99/axis-media/media.amp"
+	commandConnectionEndpoint := "http://172.24.90.99/axis-media/media.amp"
 	sessionID := uuid.New().String()
-	dataReader, _ := establishDataConnection("http://172.24.90.99/axis-media/media.amp", sessionID)
-	fmt.Println("data connection established status=%d", dataReader.Status)
-	receiveData(dataReader.Body)
+	resp, _ := establishDataConnection(dataConnectionEndpoint, sessionID)
+
+	replaceCommandConnectionIP(resp, commandConnectionEndpoint)
+	fmt.Println("data connection established status=%d", resp.Status)
+	receiveData(resp.Body)
 }
 
 func establishDataConnection(url string, sessionID string) (*http.Response, *http.Client) {
@@ -36,4 +41,32 @@ func receiveData(reader io.Reader) {
 		fmt.Println("received data from data connection", data)
 		time.Sleep(100 * time.Millisecond)
 	}
+}
+
+func establishCommandConnection(url string, sessionID string) (*http.Response, *http.Client) {
+	client := http.Client{}
+	req, err := http.NewRequest(http.MethodGet, url, http.NoBody)
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Add("x-sessioncookie", sessionID)
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	return resp, &client
+}
+
+func replaceCommandConnectionIP(resp *http.Response, commandURL string) string {
+	connectionIP := resp.Header.Get("x-server-ip-address")
+	if connectionIP == "" {
+		return commandURL
+	}
+	u, err := url.Parse(commandURL)
+	if err != nil {
+		fmt.Println(err)
+		return commandURL
+	}
+	u.Host = connectionIP
+	return u.String()
 }
