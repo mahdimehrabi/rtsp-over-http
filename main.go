9c3808b9dc114458109079409e6866b60615ad25
mahdi mehrabi <mahdimehrabi17@gmail.com>
2023-12-21 15:15:16 +0330

    send rtsp commands with seperate requests

--- main.go
+++ main.go
@@ -13,25 +13,24 @@ import (
 )
 
 func main() {
-	dataConnectionEndpoint := "http://root:admin123456@172.24.90.99/axis-media/media.amp?videocodec=h264&audio=0"
-	commandConnectionEndpoint := "http://root:admin123456@172.24.90.99/axis-media/media.amp?videocodec=h264&audio=0"
-	rtspEndpoint := "rtsp://root:admin123456@172.24.90.99/axis-media/media.amp?videocodec=h264&audio=0"
+	dataConnectionEndpoint := "http://root:admin123456@172.24.90.42/axis-media/media.amp?videocodec=h264&audio=0"
+	commandConnectionEndpoint := "http://root:admin123456@172.24.90.42/axis-media/media.amp?videocodec=h264&audio=0"
+	rtspEndpoint := "rtsp://root:admin123456@172.24.90.42/axis-media/media.amp?videocodec=h264&audio=0"
 	sessionID := uuid.New().String()
 	respData, _ := establishDataConnection(dataConnectionEndpoint, sessionID)
 	if respData.StatusCode != http.StatusOK {
 		log.Fatalf("failed to establish data connection status code %d", respData.StatusCode)
 	}
-	commandConnectionEndpoint = replaceCommandConnectionIP(respData, commandConnectionEndpoint)
 	fmt.Println("data connection established status=%d", respData.StatusCode)
-	reqBody, respCommand, _ := establishCommandConnection(commandConnectionEndpoint, sessionID)
-	if respCommand.StatusCode != http.StatusOK {
-		log.Fatalf("failed to establish command connection status code %d", respCommand.StatusCode)
-	}
+	go receiveData(respData.Body)
 
-	describeCommand(reqBody, rtspEndpoint, 1)
+	commandConnectionEndpoint = replaceCommandConnectionIP(respData, commandConnectionEndpoint)
+	_, commandClient := establishCommandConnection(commandConnectionEndpoint, sessionID)
+
+	describeCommand(commandClient, commandConnectionEndpoint, rtspEndpoint, 1)
 	fmt.Println("describe command has been sent")
 
-	receiveData(respData.Body)
+	time.Sleep(100 * time.Second)
 }
 
 func establishDataConnection(url string, sessionID string) (*http.Response, *http.Client) {
@@ -53,7 +52,7 @@ func establishDataConnection(url string, sessionID string) (*http.Response, *htt
 
 func receiveData(reader io.Reader) {
 	for {
-		data := make([]byte, 1024) // Adjust the buffer size based on your needs
+		data := make([]byte, 4096) // Adjust the buffer size based on your needs
 		n, err := reader.Read(data)
 		if err != nil {
 			if err == io.EOF {
@@ -68,7 +67,7 @@ func receiveData(reader io.Reader) {
 	}
 }
 
-func establishCommandConnection(url string, sessionID string) (*bytes.Buffer, *http.Response, *http.Client) {
+func establishCommandConnection(url string, sessionID string) (*http.Request, *http.Client) {
 	client := http.Client{}
 	reqBody := bytes.NewBuffer(make([]byte, 0))
 	req, err := http.NewRequest(http.MethodPost, url, reqBody)
@@ -78,12 +77,10 @@ func establishCommandConnection(url string, sessionID string) (*bytes.Buffer, *h
 	req.Header.Add("x-sessioncookie", sessionID)
 	req.Header.Add("Content-Length", "32767")
 	req.Header.Add("Pragma", "no-cache")
-	req.Header.Add("Accept", " application/x-rtsp-tunnelled")
-	res, err := client.Do(req)
-	if err != nil {
-		panic(err)
-	}
-	return reqBody, res, &client
+	req.Header.Add("Accept", "application/x-rtsp-tunnelled")
+	go client.Do(req)
+	time.Sleep(3 * time.Second)
+	return req, &client
 }
 
 func replaceCommandConnectionIP(resp *http.Response, commandURL string) string {
@@ -100,14 +97,24 @@ func replaceCommandConnectionIP(resp *http.Response, commandURL string) string {
 	return u.String()
 }
 
-func describeCommand(reqBody *bytes.Buffer, rtspEndpoint string, cseqValue int) {
-	encoder := base64.NewEncoder(base64.StdEncoding, reqBody)
-
+func describeCommand(client *http.Client, commandEndpoint string, rtspEndpoint string, cseqValue int) {
+	reqBody := bytes.NewBuffer(make([]byte, 0))
 	rtspCommand := fmt.Sprintf("DESCRIBE %s RTSP/1.0\r\nCSeq: %d\r\nUser-Agent: Axis AMC\r\nAccept: application/sdp\r\n\r\n", rtspEndpoint, cseqValue)
+	encoder := base64.NewEncoder(base64.StdEncoding, reqBody)
 	_, err := encoder.Write([]byte(rtspCommand))
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	req, err := http.NewRequest(http.MethodPost, commandEndpoint, reqBody)
+	if err != nil {
+		log.Fatal(err)
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(resp)
 }
 func setupCommand(reqBody *bytes.Buffer, rtspEndpoint string, cseqValue int) {
 	encoder := base64.NewEncoder(base64.StdEncoding, reqBody)
